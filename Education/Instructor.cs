0f627d71d142259fc04bf3fea8bcf9a3286957c6
Mitchell Combs <mitch.combs.92@gmail.com>
2018-05-31 15:54:23 -0400

    Updated classes and instances

---
+++ Education/Instructor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Education {
+	class Instructor {
+
+		public string Name { get; set; }
+		public int ExperienceinYears { get; set; }
+		public string PrimaryLanguage { get; set; }  //Csharp or Java
+		public string Email { get; set; }
+		public string Phone { get; set; }
+
+
+	}
+}
--- Education/Program.cs
+++ Education/Program.cs
@@ -7,6 +7,41 @@ using System.Threading.Tasks;
 namespace Education {
 	class Program {
 		static void Main(string[] args) {
+
+			Instructor Dan = new Instructor();
+			Dan.Name = "Dan";
+			Dan.ExperienceinYears = 17;
+			Dan.PrimaryLanguage = "CSharp";
+			Dan.Email = "Dan@Military.gov";
+			Dan.Phone = "123-456-7890";
+
+
+			Class dotnetbc4 = new Class();
+			dotnetbc4.Capacity = 16;
+			dotnetbc4.Code = "DN4";
+			dotnetbc4.Language = "CSharp";
+			dotnetbc4.Name = "Dont Net Boot Camp 4";
+			dotnetbc4.Instructor = Dan;
+
+
+			Student Foster = new Student();
+			Foster.Absences = 0;
+			Foster.ContactInfo = "Call Foster...";
+			Foster.FirstJob = DateTime.MinValue;
+			Foster.Name = "Foster";
+			Foster.Class = dotnetbc4;
+
+			Student JJ = new Student();
+			JJ.Absences = 0;
+			JJ.ContactInfo = "Call Jon...";
+			JJ.FirstJob = DateTime.MinValue;
+			JJ.Name = "Jonathan";
+			JJ.Class = dotnetbc4;
+
+			dotnetbc4.Students.Add(Foster);
+			dotnetbc4.Students.Add(JJ);
+
+
 		}
 	}
 }
